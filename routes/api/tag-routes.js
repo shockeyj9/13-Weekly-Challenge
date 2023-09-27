c61fcaa1feda7df18482f4a4483a4492cc990f27
Jessie Shockey <shockeyjessie92@gmail.com>
2023-09-26 23:34:03 -0600

    Create Tag Read by ID API Route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const TagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+
+    //error handling if there is no entry with that id
+    if (!TagData){
+      res.status(404).json({message: 'No Tag found with that id'});
+      return;
+    }
+
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
